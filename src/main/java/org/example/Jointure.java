810106a5fced997859009a4d20d17586a5733106
marwa sahraoui <marwasahrawi1988@gmail.com>
2022-12-21 17:42:03 +0100

    jointure method with tests

--- src/main/java/org/example/Jointure.java
+++ src/main/java/org/example/Jointure.java
@@ -2,14 +2,28 @@ package org.example;
 
 public class Jointure {
 
-    public int sum(int number){
+    public int sum(int number) {
         int result = number;
 
-        while(number / 10 > 0){
+        while (number / 10 > 0) {
             result += number % 10;
-            number = number/10;
+            number = number / 10;
         }
-        return result + number ;
+        
+        return result + number;
+    }
 
+    public int findJointure(int s1, int s2) {
+
+        while (s1 != s2) {
+            if (s1 < s2) {
+                s1 = sum(s1);
+            }
+            if (s2 < s1) {
+                s2 = sum(s2);
+            }
+        }
+
+        return s1;
     }
 }
--- src/test/java/org/example/JointureResultTest.java
+++ src/test/java/org/example/JointureResultTest.java
@@ -9,20 +9,43 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class JointureResultTest {
 
     @Test
-    public void todo(){
+    public void should_return_the_Sum_of_all_figures_of_number() {
         //arrange
-      Jointure jointure = new Jointure();
-        //act
-      int result = jointure.sum(451);
-        //assert
-      assertThat(result).isEqualTo(461);
-    }
-    @ParameterizedTest
-    @CsvSource({"451,461", "16,23", "50,55"})
-    public void _Should_generate_the_expected_int_Value(int input, int expected) {
         Jointure jointure = new Jointure();
         //act
-       int actualValue = jointure.sum( input);
+        int result = jointure.sum(451);
+        //assert
+        assertThat(result).isEqualTo(461);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"451,461", "16,23", "50,55"})
+    public void _Should_generate_the_expected_sum(int input, int expected) {
+        //arrange
+        Jointure jointure = new Jointure();
+        //act
+        int actualValue = jointure.sum(input);
+        //assert
+        assertThat(actualValue).isEqualTo(expected);
+    }
+
+    @Test
+    public void should_return_secondNumber_as_the_sum_of_all_figures_of_the_first_number() {
+        //arrange
+        Jointure jointure = new Jointure();
+        //act
+        int result = jointure.findJointure(11, 17);
+        //assert
+        assertThat(result).isEqualTo(17);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"498,507, 519", "11,17,17", "534,483, 534"})
+    public void _Should_generate_the_expected_jointure_Value(int input1, int input2, int expected) {
+        //arrange
+        Jointure jointure = new Jointure();
+        //act
+        int actualValue = jointure.findJointure(input1, input2);
         //assert
         assertThat(actualValue).isEqualTo(expected);
     }
